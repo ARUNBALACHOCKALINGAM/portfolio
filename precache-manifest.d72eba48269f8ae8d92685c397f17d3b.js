8434a12d057e72a0ddcd9bd25c8fb9ea92c02d21
ARUNBALACHOCKALINGAM <abclingam1@gmail.com>
2021-11-23 17:21:36 +0530

    Updates

---
+++ precache-manifest.d72eba48269f8ae8d92685c397f17d3b.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8694e0199c90062196c4e036ebcb226a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9c88a28a58b77de3209f",
+    "url": "/portfolio/static/css/main.2a20a325.chunk.css"
+  },
+  {
+    "revision": "a03471e4e28d5590235f",
+    "url": "/portfolio/static/js/2.5bf30cb4.chunk.js"
+  },
+  {
+    "revision": "cdffce9574fec3254f704a2dd4332202",
+    "url": "/portfolio/static/js/2.5bf30cb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba6a547a7f22cdcdc047",
+    "url": "/portfolio/static/js/3.aa94dcff.chunk.js"
+  },
+  {
+    "revision": "9c88a28a58b77de3209f",
+    "url": "/portfolio/static/js/main.52f5dd66.chunk.js"
+  },
+  {
+    "revision": "f97abc67e38c042b556c",
+    "url": "/portfolio/static/js/runtime-main.05f0848f.js"
+  },
+  {
+    "revision": "e8d9aada0cf5dd3a29753ed5f7d54e24",
+    "url": "/portfolio/static/media/3426525.e8d9aada.svg"
+  },
+  {
+    "revision": "1500b9242f28cbf5cd3146d3cef45447",
+    "url": "/portfolio/static/media/Capture.1500b924.PNG"
+  },
+  {
+    "revision": "d4b8679750caadeb19ed1673b5b797a1",
+    "url": "/portfolio/static/media/Crop_1.d4b86797.svg"
+  },
+  {
+    "revision": "9529027f7f79bafbeea2c4896ded136f",
+    "url": "/portfolio/static/media/Emoji.9529027f.svg"
+  },
+  {
+    "revision": "3ff94c31317f52ea89915abff96ebc85",
+    "url": "/portfolio/static/media/Github_black.3ff94c31.svg"
+  },
+  {
+    "revision": "77ae40daac69faa9a3a8656696853e28",
+    "url": "/portfolio/static/media/Github_white.77ae40da.svg"
+  },
+  {
+    "revision": "9a0479b28b0522c1e01e27bebf9500bd",
+    "url": "/portfolio/static/media/LinkedIN_white.9a0479b2.svg"
+  },
+  {
+    "revision": "ce32b726106d12d9c31980d1509c2e2b",
+    "url": "/portfolio/static/media/Todo.ce32b726.png"
+  },
+  {
+    "revision": "9950329eddf391c59510c284ad8a99d7",
+    "url": "/portfolio/static/media/arun.9950329e.jpg"
+  },
+  {
+    "revision": "1f671c839c91ae207597d5dbc32b8a5d",
+    "url": "/portfolio/static/media/buymeacoffee.1f671c83.svg"
+  },
+  {
+    "revision": "7fd12a51c3c349731beaaa40c31e59e9",
+    "url": "/portfolio/static/media/chat_black_24dp.7fd12a51.svg"
+  },
+  {
+    "revision": "bf2b390843693c4969dbbd06f27d3f97",
+    "url": "/portfolio/static/media/easybank.bf2b3908.PNG"
+  },
+  {
+    "revision": "e546fed390d97b35362f0eb106595af7",
+    "url": "/portfolio/static/media/frontend.e546fed3.svg"
+  },
+  {
+    "revision": "34e76ed14abac44a660549dbcb29e653",
+    "url": "/portfolio/static/media/paint.34e76ed1.svg"
+  },
+  {
+    "revision": "5c5277f1109bdd6710f173a57fb54273",
+    "url": "/portfolio/static/media/settings.5c5277f1.svg"
+  }
+]);
